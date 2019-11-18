16e53a9384ad2e0c1dd49763053def93b81f3898
kwangQ <km_yoo007@naver.com>
2019-11-18 03:40:42 +0000

    initial commit

---
+++ src/main/java/net/sophi/MySophiTerminalApplication.java
@@ -0,0 +1,13 @@
+package net.sophi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySophiTerminalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySophiTerminalApplication.class, args);
+	}
+
+}
